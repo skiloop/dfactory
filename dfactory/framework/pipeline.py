69979b9fde45f8c05d933540df57a5a5a7a6b27a
skiloop <skiloop@gmail.com>
2022-05-14 15:46:26 +0800

    refactor: add comments and change variable names

--- dfactory/framework/pipeline.py
+++ dfactory/framework/pipeline.py
@@ -1,51 +1,89 @@
 #!/usr/bin/env python
 # coding=utf-8
+#
+#
 from typing import List, Dict
 
-from .handlerbase import Handler
 from .common import new_operator_from_dict
+from .handlerbase import Handler
 from .seeders import Seeder
 
 
 class Pipeline:
+    """
+    data pipeline class
+    """
 
     def __init__(self):
         self.operators = []
 
     def handle(self, item):
+        """
+        handle over operators
+        :param item: object to handle
+        :return: the handled object
+        """
         obj = item
-        for c in self.operators:
-            obj = c.handle(obj)
+        for operator in self.operators:
+            obj = operator.handle(obj)
             if obj is None:
                 break
         return obj
 
     def exit(self):
-        for c in self.operators:
-            if hasattr(c, 'on_exit'):
-                c.on_exit()
+        """
+        action on exit
+        :return: None
+        """
+        for operator in self.operators:
+            if hasattr(operator, 'on_exit'):
+                operator.on_exit()
 
     def enter(self):
-        for c in self.operators:
-            if hasattr(c, 'on_enter'):
-                c.on_exit()
+        """
+        action before pipeline operator starts up
+        :return: None
+        """
+        for operator in self.operators:
+            if hasattr(operator, 'on_enter'):
+                operator.on_exit()
 
     def add(self, operator: Handler):
+        """
+        add new handler
+        :param operator: object of Handler
+        :return: None
+        """
         self.operators.append(operator)
 
     def load_from_dict(self, data: List[Dict]):
-        for c in data:
-            obj = new_operator_from_dict(c)
+        """
+        load operators from dict data
+        :param data: operators config
+        :return: None
+        """
+        for cfg in data:
+            obj = new_operator_from_dict(cfg)
             if obj is not None:
                 self.add(obj)
 
     @staticmethod
     def from_dict(data: List[Dict]):
-        p = Pipeline()
-        p.load_from_dict(data)
-        return p
+        """
+        create Pipeline object from dict data
+        :param data: pipeline config
+        :return: a new Pipeline object
+        """
+        pipeline = Pipeline()
+        pipeline.load_from_dict(data)
+        return pipeline
 
     def iter(self, seeder: Seeder):
+        """
+        seeding pipeline and handle item
+        :param seeder: pipeline seeder
+        :return: item generator
+        """
         for item in seeder.iter():
             if item is None:
                 break
@@ -55,6 +93,12 @@ class Pipeline:
             yield obj
 
     def run(self, seeder: Seeder, fun=None):
+        """
+        start pipeline
+        :param seeder: pipeline seeder
+        :param fun: item operator out of pipeline
+        :return: None
+        """
         self.enter()
         for item in self.iter(seeder):
             if fun:
